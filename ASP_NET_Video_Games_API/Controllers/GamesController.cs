45f7b02140eed9ce929ea198a4c3f50c313dfea5
tommychong316 <chong316@gmail.com>
2022-04-27 17:54:38 -0400

    created function that would retrieve all video games

--- ASP_NET_Video_Games_API/Controllers/GamesController.cs
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -15,12 +15,13 @@ namespace ASP_NET_Video_Games_API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetPublishers()
+        public IActionResult GetGames()
         {
-            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+            var allVideoGames = _context.VideoGames.Select(vg => vg).Distinct();
 
-            return Ok(videoGamePublishers);
+            return Ok(allVideoGames);
         }
+
         [HttpGet("{id}")]
         public IActionResult GetGamesByPublisher(int id)
         {
